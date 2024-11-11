fbec2023ba9ed0d4f8ba6cf17aaede7382478c28
HamzaShah1212 <hamzashahsyed92@gmail.com>
2024-11-11 23:28:59 +0500

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './layout/**/*.liquid',  // All .liquid files in the layout folder
-    './src/**/*.html',       // All HTML files in src folder
-    './src/**/*.liquid',     // All Liquid files in src folder
-    './assets/**/*.js',      // All JS files in assets folder
-    './components/**/*.liquid',  // Additional folders if you have other Liquid files
-    './**/*.liquid',  
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/*.liquid'
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
